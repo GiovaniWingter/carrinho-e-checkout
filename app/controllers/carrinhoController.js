6e5a53b7789a83c997d5ffc85a70f5e546f3674d
Giovani <giovaniwingter@hotmail.com>
2024-08-16 09:14:54 -0300

    first commit

---
+++ app/controllers/carrinhoController.js
@@ -0,0 +1,72 @@
+const { carrinho } = require("../util/carrinho");
+const { body, validationResult } = require("express-validator");
+
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const https = require('https');
+
+
+const carrinhoController = {
+
+    addItem: (req, res) => {
+        try {
+            let id = req.query.id;
+            let preco = req.query.preco;
+            carrinho.addItem(id, 1, preco);
+            carrinho.atualizarCarrinho(req);
+            let url = req.rawHeaders[25];
+            let urlChamadora = url.replace("http://localhost:3000", "");
+            res.redirect(urlChamadora);
+        } catch (e) {
+            console.log(e);
+            res.render("pages/cadastro", {
+                listaErros: erros, dadosNotificacao: {
+                    titulo: "Erro ao cadastrar!", mensagem: "Verifique os valores digitados!", tipo: "error"
+                }, valores: req.body
+            })
+        }
+    },
+
+    removeItem: (req, res) => {
+        try {
+            let id = req.query.id;
+            let qtde = req.query.qtde;
+            carrinho.removeItem(id, qtde);
+            carrinho.atualizarCarrinho(req);           
+            let url = req.rawHeaders[25];
+            let urlChamadora = url.replace("http://localhost:3000", "");
+            res.redirect(urlChamadora);
+        } catch (e) {
+            console.log(e);
+            res.render("pages/login", {
+                listaErros: null,
+                dadosNotificacao: { titulo: "Falha ao logar!", mensagem: "Usuário e/ou senha inválidos!", tipo: "error" }
+            })
+        }
+
+
+    },
+
+    listarcarrinho: (req, res) => {
+        try {
+            console.log("teste")
+            carrinho.atualizarCarrinho(req);
+            req.session.autenticado.login = req.query.login;
+            res.render("pages/listar-carrinho", {
+                autenticado: req.session.autenticado,
+                carrinho: req.session.carrinho,
+                listaErros: null,
+            });
+        } catch (e) {
+            console.log(e);
+            res.render("pages/listar-carrinho", {
+                autenticado: req.session.autenticado,
+                carrinho: null,
+                listaErros: null,
+                dadosNotificacao: { titulo: "Falha ao Listar Itens !", mensagem: "Erro interno no servidor!", tipo: "error" }
+            })
+        }
+    },
+}
+
+
+module.exports = {carrinhoController}
---
+++ app/routes/router.js
@@ -0,0 +1,104 @@
+var express = require("express");
+var router = express.Router();
+
+const {
+  verificarUsuAutenticado,
+  limparSessao,
+  gravarUsuAutenticado,
+  verificarUsuAutorizado,
+} = require("../models/autenticador_middleware");
+
+const {usuarioController} = require("../controllers/usuarioController");
+const {carrinhoController} = require("../controllers/carrinhoController");
+const {hqController} = require("../controllers/hqController");
+
+const uploadFile = require("../util/uploader")("./app/public/imagem/perfil/");
+// const uploadFile = require("../util/uploader")();
+
+router.get("/addItem", function (req, res) {
+  carrinhoController.addItem(req, res);
+});
+
+router.get("/removeItem", function (req, res) {
+  carrinhoController.removeItem(req, res);
+});
+
+router.get("/listar-carrinho", function (req, res) {
+  carrinhoController.listarcarrinho(req,res);
+});
+
+router.get(
+  "/perfil",
+  verificarUsuAutorizado([1, 2, 3], "pages/restrito"),
+  async function (req, res) {
+    usuarioController.mostrarPerfil(req, res);
+  }
+);
+
+router.post(
+  "/perfil",
+  uploadFile("imagem-perfil_usu"),
+  usuarioController.regrasValidacaoPerfil,
+  verificarUsuAutorizado([1, 2, 3], "pages/restrito"),
+  async function (req, res) {
+    usuarioController.gravarPerfil(req, res);
+  }
+);
+
+
+router.get("/", verificarUsuAutenticado, function (req, res) {
+  hqController.listar(req, res);
+});
+
+router.get("/sair", limparSessao, function (req, res) {
+  res.redirect("/");
+});
+
+router.get("/login", function (req, res) {
+  res.render("pages/login", { listaErros: null, dadosNotificacao: null });
+});
+
+router.post(
+  "/login",
+  usuarioController.regrasValidacaoFormLogin,
+  gravarUsuAutenticado,
+  function (req, res) {
+    usuarioController.logar(req, res);
+  }
+);
+
+router.get("/cadastro", function (req, res) {
+  res.render("pages/cadastro", {
+    listaErros: null,
+    dadosNotificacao: null,
+    valores: { nome_usu: "", nomeusu_usu: "", email_usu: "", senha_usu: "" },
+  });
+});
+
+router.post(
+  "/cadastro",
+  usuarioController.regrasValidacaoFormCad,
+  async function (req, res) {
+    usuarioController.cadastrar(req, res);
+  }
+);
+
+router.get(
+  "/adm",
+  verificarUsuAutenticado,
+  verificarUsuAutorizado([2, 3], "pages/restrito"),
+  function (req, res) {
+    res.render("pages/adm", req.session.autenticado);
+  }
+);
+
+router.get("/favoritar", verificarUsuAutenticado,function (req, res) {
+    hqController.favoritar(req, res);
+  }
+);
+
+
+
+
+
+module.exports = router;
